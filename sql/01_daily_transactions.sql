edbcb57584c552e1496ac6954363a1360c7c24d9
Luciano Mosquén <luciano.mosquen@gmail.com>
2026-04-09 09:43:58 -0300

    Create 01_daily_transactions.sql

---
+++ sql/01_daily_transactions.sql
@@ -0,0 +1,7 @@
+SELECT
+  DATE(block_timestamp) AS fecha,
+  COUNT(*) AS transacciones
+FROM `bigquery-public-data.crypto_ethereum.transactions`
+WHERE DATE(block_timestamp) >= DATE_SUB(CURRENT_DATE(), INTERVAL 90 DAY)
+GROUP BY fecha
+ORDER BY fecha;
